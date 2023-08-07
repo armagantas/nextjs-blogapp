e56e2eadf16dbd9084643fa12ede27dc3784d86f
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-08-07 15:41:39 +0300

    Commit
    Portfolio page is complete. Extensions have been drafted.

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,60 @@
 import React from "react";
 import styles from "./page.module.css";
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  console.log(params);
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/illustration1.jpg"
+            alt=""
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/illustration1.jpg"
+            alt=""
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/illustration1.jpg"
+            alt=""
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
