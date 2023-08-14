c9b739f4cbacd2f4930b60b22113d48b19ce9642
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-08-14 15:29:11 +0300

    Commit

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connect = () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (error) {
     throw new Error("Connection failed!");
