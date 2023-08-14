f5dd642ca0ec12e4d6c81cee6cc535e064c03768
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-08-14 14:58:59 +0300

    Commit

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,13 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-};
+});
 
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,8 +1,19 @@
+"use client";
+
 import React from "react";
 import styles from "./page.module.css";
+import { signIn, useSession } from "next-auth/react";
 
 const Login = () => {
-  return <div className={styles.container}>Login</div>;
+  const { data: session } = useSession();
+  console.log(session);
+  return (
+    <div className={styles.container}>
+      <button onClick={() => signIn("github")}>
+        Login with Github Account
+      </button>
+    </div>
+  );
 };
 
 export default Login;
