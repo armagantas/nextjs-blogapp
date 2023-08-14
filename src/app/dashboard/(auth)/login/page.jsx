7bf26a2531e0274a6e7425ba3132e3e447363bd0
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-08-14 16:00:16 +0300

    Commit

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -5,10 +5,33 @@ import styles from "./page.module.css";
 import { signIn, useSession } from "next-auth/react";
 
 const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn("credentials", { email, password });
+  };
+
   const { data: session } = useSession();
   console.log(session);
   return (
     <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+      </form>
       <button onClick={() => signIn("github")}>
         Login with Github Account
       </button>
